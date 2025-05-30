9c106cc45e859d67f40cdb80bf4d86b970a24248
Francisco David Martinez Martinez <franciscodavidmtzmtz@gmail.com>
2025-05-29 23:11:04 -0600

    dividiendo los inserts

--- src/scripts-03-crea-app-container/e-10-app-container.sql
+++ src/scripts-03-crea-app-container/e-10-app-container.sql
@@ -135,24 +135,12 @@ Prompt ================================
 Pause [ENTER] para continuar o Ctrl + C para cancelar
 @e-11-creacion-tablas-fk.sql
 
-------------------------------------------
-INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (1, 'Nuevo Leon');
-INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (2, 'CDMX');
-INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (3, 'Quintana Roo');
-INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (4, 'Tabasco');
-INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (5, 'Tamaulipas');
+Prompt ================================
+Prompt Ejecutando e-12-catalogos-insert.sql
+Prompt ================================
+Pause [ENTER] para continuar o Ctrl + C para cancelar
+@e-12-catalogos-insert.sql
 
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (1, 'Vieja República Demo', 1);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (2, 'Nueva Togo', 4);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (3, 'Vieja Guinea', 5);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (4, 'San Emilia los altos', 3);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (5, 'San Sara los altos', 1);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (6, 'San Violeta de la Mo', 3);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (7, 'San Rufino los altos', 1);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (8, 'San Augusto los bajo', 3);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (9, 'San Gilberto los alt', 2);
-INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (10, 'Nueva Sri Lanka', 1);
----------------------------------------
 
 Prompt ============{ 5. Terminando Configuraicon de la App }=================
 connect sys/system2@pet_care_ac  as sysdba
---
+++ src/scripts-03-crea-app-container/e-12-catalogos-insert.sql
@@ -0,0 +1,31 @@
+--@Autor: Martinez Martinez Francisco David
+--@Fecha creación: 27/05/2025
+--@Descripción: Poblado de Tablas con Inserts
+
+SET SERVEROUTPUT ON;
+BEGIN
+INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (1, 'Nuevo Leon');
+INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (2, 'CDMX');
+INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (3, 'Quintana Roo');
+INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (4, 'Tabasco');
+INSERT INTO PET_CARE_ADMIN.ESTADO (clvEstado, nombreEstado) VALUES (5, 'Tamaulipas');
+
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (1, 'Vieja República Demo', 1);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (2, 'Nueva Togo', 4);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (3, 'Vieja Guinea', 5);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (4, 'San Emilia los altos', 3);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (5, 'San Sara los altos', 1);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (6, 'San Violeta de la Mo', 3);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (7, 'San Rufino los altos', 1);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (8, 'San Augusto los bajo', 3);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (9, 'San Gilberto los alt', 2);
+INSERT INTO PET_CARE_ADMIN.MUNICIPIO (clvMunicipio, nombreMunicipio, clvEstado) VALUES (10, 'Nueva Sri Lanka', 1);
+
+COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('En línea: ' || DBMS_UTILITY.FORMAT_ERROR_BACKTRACE);
+END;
+/
